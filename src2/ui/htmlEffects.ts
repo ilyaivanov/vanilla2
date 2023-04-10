5e1ed55130f8d849921fe5899103b26b0653afaa
Ilya Ivanov <static.ila@gmail.com>
2023-04-10 11:30:46 +0300

    Add animations revertability

--- src2/ui/htmlEffects.ts
+++ src2/ui/htmlEffects.ts
@@ -7,15 +7,19 @@ import { Item } from "../core";
 export function closeItem(item: Item) {
   item.isOpen = false;
   const children = ids.getChildren(item)!;
-
-  collapse(children);
+  if (!revertAnimations(children))
+    animateHeight(item, children, children.clientHeight, 0);
 }
 
 export function openItem(item: Item) {
   item.isOpen = true;
+
+  const existingChildren = ids.getChildren(item);
+  if (existingChildren && revertAnimations(existingChildren)) return;
+
   const children = viewChildren(item);
   ids.getItem(item)?.insertAdjacentElement("afterend", children);
-  expand(children);
+  animateHeight(item, children, 0, children.clientHeight);
 }
 
 export function removeItemFromDom(item: Item) {
@@ -26,6 +30,8 @@ export function itemAddedAt(parent: Item, index: number) {
   const childrenElem = ids.getChildren(parent);
   const newNode = viewItem(parent.children[index]);
   childrenElem?.insertBefore(newNode, childrenElem.children[index]);
+
+  //TODO: animate
 }
 
 //
@@ -35,7 +41,7 @@ export const ids = {
   getChildren: (item: Item) => document.getElementById("c" + item.id),
 
   text: (item: Item) => "text-" + item.id,
-  getText: (item: Item) => document.getElementById("text-" + item.id),
+  getText: (item: Item) => document.getElementById(ids.text(item)),
 
   container: (item: Item) => "container-" + item.id,
   getContainer: (item: Item) => document.getElementById("container-" + item.id),
@@ -44,28 +50,35 @@ export const ids = {
   getItem: (item: Item) => document.getElementById(item.id),
 };
 
+//
+//
+//
 // animations
-
-function collapse(elem: HTMLElement) {
-  const height = elem.clientHeight;
+function animateHeight(
+  item: Item,
+  elem: HTMLElement,
+  from: number,
+  to: number
+) {
   elem.style.overflowY = "hidden";
   elem
-    .animate([{ height: height + "px" }, { height: "0px" }], {
-      duration: Math.max(200, height / 2),
+    .animate([{ height: from + "px" }, { height: to + "px" }], {
+      duration: Math.max(200, Math.abs(to - from) / 2),
       easing: "ease-in-out",
-    })
-    .addEventListener("finish", () => elem.remove());
-}
-
-function expand(elem: HTMLElement) {
-  const height = elem.clientHeight;
-  elem.style.overflowY = "hidden";
-  elem
-    .animate([{ height: "0px" }, { height: height + "px" }], {
-      duration: Math.max(200, height / 2),
-      easing: "ease-in",
     })
     .addEventListener("finish", () => {
       elem.style.removeProperty("overflow-y");
+      if (!item.isOpen) elem.remove();
     });
+}
+
+function revertAnimations(elem: HTMLElement): boolean {
+  const animations = elem.getAnimations();
+  console.log(animations.length);
+  if (animations.length == 0) return false;
+
+  for (let i = 0; i < animations.length; i += 1) {
+    animations[i].reverse();
+  }
+  return true;
 }
